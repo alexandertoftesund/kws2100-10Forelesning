4832da7434bd77268b09275adfbaaefe67d274db
Alexander Toftesund <alexandertoftesund@gmail.com>
2026-03-16 16:21:58 +0100

    Serve the app to hono test 2

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,9 @@ import { serve } from "@hono/node-server";
 import { serveStatic } from "@hono/node-server/serve-static";
 
 const app = new Hono();
-//app.get("/", (c) => c.text("Hello World 9000"));
+app.get("/api/test", (c) => {
+  console.log("Testing");
+  return c.text("Hello World 9000");
+});
 app.get("*", serveStatic({ root: "../dist" }));
 serve(app);
