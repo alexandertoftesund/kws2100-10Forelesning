baddf204b10a302801a4399fcef643b0959badf9
Alexander Toftesund <alexandertoftesund@gmail.com>
2026-03-23 23:36:12 +0100

    Serve the app to hono test 2

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,30 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
-import { serveStatic } from "@hono/node-server/serve-static";
+import pg from "pg";
 
 const app = new Hono();
-app.get("/api/test", (c) => {
-  console.log("Testing");
-  return c.text("Hello World 9000");
+
+const postgresql = new pg.Pool({
+  user: "postgres",
+  password: "password",
+  database: "postgres",
+  host: "localhost",
+  port: 5432,
 });
-app.get("*", serveStatic({ root: "../dist" }));
+
+app.get("/api/grunnskoler", async (c) => {
+  const result = await postgresql.query(
+    `select  skolenavn, eierforhold, antallelever, posisjon::json as geometry
+     from grunnskoler_d3dd22a6be80438d9f44b0afa9b82b1b.grunnskole s
+     `,
+  );
+  return c.json({
+    type: "FeatureCollection",
+    features: result.rows.map(({ geometry, ...properties }) => ({
+      type: "Feature",
+      properties,
+      geometry,
+    })),
+  });
+});
 serve(app);
--- src/components/app/application.tsx
+++ src/components/app/application.tsx
@@ -6,19 +6,55 @@ import { useGeographic } from "ol/proj.js";
 
 // @ts-ignore
 import "ol/ol.css";
+import VectorLayer from "ol/layer/Vector.js";
+import VectorSource from "ol/source/Vector.js";
+import { GeoJSON } from "ol/format.js";
+
+import proj4 from "proj4";
+import { register } from "ol/proj/proj4.js";
+import { Style, Circle as CircleStyle, Fill, Stroke } from "ol/style.js";
+
+proj4.defs(
+  "EPSG:25833",
+  "+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs",
+);
+register(proj4);
 
 useGeographic();
 
+const kommuneLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "/geojson/Kommuner-S.geojson",
+    format: new GeoJSON(),
+  }),
+});
+
+const grunnskoleStyle = new Style({
+  image: new CircleStyle({
+    radius: 6,
+    fill: new Fill({ color: "#ff4500" }),
+    stroke: new Stroke({
+      color: "#ffffff",
+      width: 2,
+    }),
+  }),
+});
+
+const grunnskoleLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "/api/grunnskoler",
+    format: new GeoJSON({ dataProjection: "EPSG:25833" }),
+  }),
+  style: grunnskoleStyle,
+});
+
 const map = new Map({
-  view: new View({ center: [10.7, 59.9], zoom: 12 }),
-  layers: [new TileLayer({ source: new OSM() })],
+  view: new View({ center: [10.7, 59.9], zoom: 8 }),
+  layers: [new TileLayer({ source: new OSM() }), kommuneLayer, grunnskoleLayer],
 });
 
 export function Application() {
   const mapRef = useRef<HTMLDivElement | null>(null);
-  useEffect(() => {
-    map.setTarget(mapRef.current!);
-  }, []);
-
+  useEffect(() => map.setTarget(mapRef.current!), []);
   return <div ref={mapRef}></div>;
 }
